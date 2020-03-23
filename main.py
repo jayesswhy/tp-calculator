733e59d758cada6bc0adf69a0645b60841860b1d
jayesswhy <nica.yoo@hotmail.com>
2020-03-24 00:43:22 +1100

    Added validation to prevent non-numeric data from being entered

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-# this program calculates the amount of toilet paper required during quarantine
+# This program calculates the amount of toilet paper required during quarantine
 
-# usage
-poops = int(input("How many poops per day?: ")) # poops per day
-wipes = int(input("How many wipes per poop?: ")) # wipes per poop
-sheets = int(input("How many sheets per wipe?: ")) # sheets per wipe
+def getDigit (question):
+    while True:
+        answer = input(question)
+        if answer.isdigit():
+            return int(answer)
+        else:
+            print("Please enter a number")
+
+def getPositiveDigit (question):
+    while True:
+        answer = getDigit(question)
+        if answer <= 0:
+            print("Enter a number greater than 1")
+        else:
+            return answer
+
+
+poops = getDigit("How many poops per day?: ")
+wipes = getDigit("How many wipes per poop?: ")
+sheets = getDigit("How many sheets per wipe?: ")
 poops_total = poops * (wipes * sheets)
 
-# pees
-pees = int(input("How many times do you pee per day?: ")) # pees per day
-sheets1 = int(input("How many sheets per pee?: ")) # sheets per pee
+pees = getDigit("How many times do you pee per day?: ")
+sheets1 = getDigit("How many sheets per pee?: ")
 pees_total = pees * sheets1
 
-# how many people in household
-people = int(input("How many people in your household?: "))
+people = getPositiveDigit("How many people in your household?: ")
 sheets_total = people * (pees_total + poops_total)
 
-# sheets per roll of toilet paper
-roll = int(input("How many sheets per roll of toilet paper?: "))
+roll = getPositiveDigit("How many sheets per roll of toilet paper?: ")
 
-# quarantine period
-quarantine = int(input("How many days quarantine?: "))
+quarantine = getDigit("How many days quarantine?: ")
 
-# how many rolls should you buy for quarantine
 total = int((sheets_total * quarantine) / roll)
-print ("You should buy", total, "rolls of toilet paper")
+print("You should buy", total, "rolls of toilet paper")
